96f4bbd86d5e73c0fc7401f4cf188cb24870ed13
tuhindewan <tuhinsshadow@gmail.com>
2021-04-12 16:51:44 +0600

    CAAB-71: User account management - Login logic build

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,24 +10,37 @@
       <div class="card-body">
         <p class="login-box-msg">Sign in to start your session</p>
 
-        <form id="loginForm">
+        <form id="loginForm" method="POST" action="{{ route('login') }}">
+            @csrf
             <div class="form-group input-group">
-                <input type="text" name="username" id="username"
-                  class="form-control" placeholder="Enter Username">
+                <input id="username" type="text"
+                    class="form-control @error('username') is-invalid @enderror"
+                    name="username" value="{{ old('username') }}" placeholder="Enter Username">
                 <div class="input-group-append">
                     <div class="input-group-text">
                         <span class="fas fa-envelope"></span>
                     </div>
                 </div>
+                @error('username')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
             <div class="form-group input-group">
-                <input type="password" name="password" id="password"
-                    class="form-control" placeholder="Password">
+                <input id="password" type="password"
+                    class="form-control @error('password') is-invalid @enderror"
+                    name="password" placeholder="Enter Password">
                 <div class="input-group-append">
                     <div class="input-group-text">
                         <span class="fas fa-lock"></span>
                     </div>
                 </div>
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
             <div class="row">
                 <div class="col-8">
