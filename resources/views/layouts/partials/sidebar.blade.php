09321f1ba0e31c642c31b4ddbb9056da858f013e
tuhindewan <tuhinsshadow@gmail.com>
2021-04-18 12:28:46 +0600

    CAAB-80: Some issues changes from last sprint 	- Left menu dynamic active and css add

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,13 +1,10 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
     <img src="{{ asset('img/caab-logo.jpeg') }}" alt="caab logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">HCC</span>
     </a>
 
-    <!-- Sidebar -->
     <div class="sidebar">
-    <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
         <img src="{{ asset('img/profile.png') }}" class="img-circle elevation-2" alt="User Image">
@@ -17,7 +14,6 @@
         </div>
     </div>
 
-    <!-- SidebarSearch Form -->
     <div class="form-inline">
         <div class="input-group" data-widget="sidebar-search">
         <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
@@ -29,7 +25,6 @@
         </div>
     </div>
 
-    <!-- Sidebar Menu -->
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
@@ -38,7 +33,10 @@
                     <p>Dashboard</p>
                 </a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item {{ Route::is('admin.employee.create') ||
+                                    Route::is('admin.employees.index') ||
+                                    Route::is('admin.employee.edit') ||
+                                    Route::is('admin.employee.show') ? 'menu-open' : '' }}">
                 <a href="#" class="nav-link">
                     <i class="nav-icon fas fa-cogs text-green"></i>
                     <p>
@@ -47,7 +45,10 @@
                     </p>
                 </a>
                 <ul class="nav nav-treeview">
-                    <li class="nav-item">
+                    <li class="nav-item {{ Route::is('admin.employee.create') ||
+                                            Route::is('admin.employees.index') ||
+                                            Route::is('admin.employee.edit') ||
+                                            Route::is('admin.employee.show') ? 'active_menu' : '' }}">
                         <a href="{{ route('admin.employees.index') }}" class="nav-link">
                             <i class="fas fa-users nav-icon"></i>
                             <p>Employees</p>
@@ -55,7 +56,7 @@
                     </li>
                 </ul>
             </li>
-            <li class="nav-item">
+            <li class="nav-item {{ Route::is('admin.profile.getProfileData') ? 'active_menu' : '' }}">
                 <a href="{{ route('admin.profile.getProfileData') }}" class="nav-link">
                     <i class="nav-icon fas fa-user text-yellow"></i>
                     <p>Profile</p>
@@ -76,8 +77,6 @@
             </li>
         </ul>
     </nav>
-    <!-- /.sidebar-menu -->
     </div>
-    <!-- /.sidebar -->
 </aside>
 
